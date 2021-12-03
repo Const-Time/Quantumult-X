df9effe2217f4df6eb6328addc07a24bf5098e9e
Const-Time <57440905+Const-Time@users.noreply.github.com>
2021-12-04 00:30:44 +0800

    Create response.js

---
+++ response.js
@@ -0,0 +1,176 @@
+var body = '{
+  "tel": "",
+  "addr": "成华区建设南新路80号",
+  "cname": "成都市成华区猛追湾建设路社区卫生服务中心",
+  "lat": 30.665094,
+  "lng": 104.104937,
+  "distance": 0,
+  "payment": {
+    "alipay": "true",
+    "WechatPay": "true",
+    "UnionPay": "true",
+    "cashier": "true"
+  },
+  "BigPic": "https://app.zhifeishengwu.com/img/none.png",
+  "IdcardLimit": true,
+  "notice": "九价宫颈癌疫苗预约请先关注本单位微信公众号（成华猛追建设路社区卫生服务中心）。",
+  "status": 200,
+  "list": [
+    {
+      "id": 1,
+      "text": "九价人乳头瘤病毒疫苗",
+      "price": "1318.00",
+      "descript": "九价简介：接种九价宫颈癌疫苗可以预防人乳头瘤病毒6、11、16、18、31、33、45、52、58型感染引起的尖锐湿疣、癌前病变、原位腺癌和宫颈癌，是目前覆盖HPV型别最多的HPV疫苗。全程三针， 适用于16-26岁女性。",
+      "warn": "适用于16-26岁女性。",
+      "tags": [
+        "进口",
+        "默沙东",
+        "16-26",
+        "3针次"
+      ],
+      "questionnaireId": 0,
+      "remarks": "身体健康前提下带上身份证来接种。",
+      "NumbersVaccine": [
+        {
+          "cname": "第一针",
+          "value": 1
+        },
+        {
+          "cname": "第二针",
+          "value": 2
+        },
+        {
+          "cname": "第三针",
+          "value": 3
+        }
+      ],
+      "date": "12-04 12:00 至 12-04 12:20",
+      "BtnLable": "暂未开始",
+      "enable": true
+    },
+    {
+      "id": 2,
+      "text": "四价人乳头瘤病毒疫苗",
+      "price": "818.00",
+      "descript": "四价简介：接种四价宫颈癌疫苗可以预防人乳头瘤病毒16、18两种高危亚型感染，以及人乳头瘤6、11低危亚型感染。可预防上述亚型引起的尖锐湿疣和宫颈癌。全程三针，适用于9-45岁女性。",
+      "warn": "适用于9-45岁女性。",
+      "tags": [
+        "进口",
+        "默沙东",
+        "9-45",
+        "3针次"
+      ],
+      "questionnaireId": 0,
+      "remarks": "身体健康且带上身份证",
+      "NumbersVaccine": [
+        {
+          "cname": "第一针",
+          "value": 1
+        },
+        {
+          "cname": "第二针",
+          "value": 2
+        },
+        {
+          "cname": "第三针",
+          "value": 3
+        }
+      ],
+      "date": "暂无",
+      "BtnLable": "暂未开始",
+      "enable": false
+    },
+    {
+      "id": 3,
+      "text": "二价人乳头瘤病毒疫苗-进口",
+      "price": "600.00",
+      "descript": "二价简介：接种二价宫颈癌疫苗可以预防人乳头瘤病毒16、18两种高危亚型感染，可预防上述亚型引起的宫颈癌。全程三针，适用于9-45岁女性。",
+      "warn": "适用于9-45岁女性。",
+      "tags": [
+        "进口",
+        "葛兰素史克",
+        "9-45",
+        "3针次"
+      ],
+      "questionnaireId": 0,
+      "remarks": "身体健康且带上本人身份证",
+      "NumbersVaccine": [
+        {
+          "cname": "第一针",
+          "value": 1
+        },
+        {
+          "cname": "第二针",
+          "value": 2
+        },
+        {
+          "cname": "第三针",
+          "value": 3
+        }
+      ],
+      "date": "暂无",
+      "BtnLable": "暂未开始",
+      "enable": false
+    },
+    {
+      "id": 54,
+      "text": "二价人乳头瘤病毒疫苗(大肠杆菌)",
+      "price": "349.00",
+      "descript": "",
+      "warn": "",
+      "tags": [
+        "国产",
+        "9-45"
+      ],
+      "questionnaireId": 0,
+      "remarks": "身体健康且带上本人身份证",
+      "NumbersVaccine": [
+        {
+          "cname": "第一针",
+          "value": 1
+        },
+        {
+          "cname": "第二针",
+          "value": 2
+        },
+        {
+          "cname": "第三针",
+          "value": 3
+        }
+      ],
+      "date": "12-02 00:00 至 12-10 00:00",
+      "enable": true,
+      "BtnLable": "立即预约"
+    },
+    {
+      "id": 62,
+      "text": "四价流感病毒裂解疫苗",
+      "price": "148.00",
+      "descript": "",
+      "warn": "",
+      "tags": [
+        "华兰"
+      ],
+      "questionnaireId": 0,
+      "remarks": "鸡蛋过敏不能接种，请勿预约",
+      "NumbersVaccine": [
+        {
+          "cname": "第一针",
+          "value": 1
+        },
+        {
+          "cname": "第二针",
+          "value": 2
+        },
+        {
+          "cname": "第三针",
+          "value": 3
+        }
+      ],
+      "date": "12-02 00:00 至 12-31 00:00",
+      "enable": true,
+      "BtnLable": "立即预约"
+    }
+  ]
+}';
+$done(body);
